99b01821f2f7eb5f620ea9eede7de1be762f06e7
writersblockchain <seanmconrad94@gmail.com>
2024-01-22 11:36:08 -0500

    remove config

--- polygon/hardhat.config.js
+++ polygon/hardhat.config.js
@@ -13,9 +13,7 @@ module.exports = {
     },
   },
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-    },
+    url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+    accounts: [process.env.PRIVATE_KEY],
   },
 };
