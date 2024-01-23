9e870df066833da0ed9af26009b8b957ba49354d
writersblockchain <seanmconrad94@gmail.com>
2024-01-23 18:24:54 -0500

    polygon

--- polygon/hardhat.config.js
+++ polygon/hardhat.config.js
@@ -13,8 +13,8 @@ module.exports = {
     },
   },
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+    polygon: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
